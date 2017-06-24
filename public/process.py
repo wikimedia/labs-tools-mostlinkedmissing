730eaf8e1075ca643f00b4a8959c4d057f30a99d
Martin Urbanec <martin.urbanec@wikimedia.cz>
2017-06-24 16:35:46 +0200

    Add all

--- public/process.py
+++ public/process.py
@@ -23,7 +23,7 @@ print """
                 <title>Chybějící stránky</title>
         </head>
         <body>
-		<p><a href="index.html">Zpět</a>
+		<p><a href="index.php">Zpět</a>
 """
 ###############FUNCTIONS######################
 #Print end header
