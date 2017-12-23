bf252d5e0fd30eccb6db5fbafbabe73689794dd2
Martin Urbanec <martin.urbanec@webappky.cz>
2017-12-23 22:55:49 +0100

    Use generator to reduce memory-usage

--- generator/update.py
+++ generator/update.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
+from __future__ import generators
 from wmflabs import db
 
-# Get the data from database
-conn = db.connect('cswiki')
-with conn.cursor() as cur:
-	sql = "SELECT pl_namespace AS namespace, pl_title AS title, COUNT(*) AS value FROM pagelinks LEFT JOIN page AS pg1 ON pl_namespace = pg1.page_namespace AND pl_title = pg1.page_title LEFT JOIN page AS pg2 ON pl_from = pg2.page_id WHERE pg1.page_namespace IS NULL AND pl_namespace NOT IN ( 2, 3 ) AND pg2.page_namespace NOT IN ( 8, 10 ) GROUP BY pl_namespace, pl_title ORDER BY value;"
-	cur.execute(sql)
-	data = cur.fetchall()
+def ResultIter(cursor, arraysize=1000):
+	 'An iterator that uses fetchmany to keep memory usage down'
+	 while True:
+	 	results = cursor.fetchmany(arraysize)
+		if not results:
+			break
+		for result in results:
+			yield result
 
+# Prepare database for storing
 
-# Store the data into tools-db
 conn = db.connect('s53348__mostLinkedMissing')
 with conn.cursor() as cur:
 	sql = 'drop table if exists mostLinkedMissingNew;'
@@ -25,8 +28,14 @@ with conn.cursor() as cur:
 	sql = 'set charset utf8;'
 	cur.execute(sql)
 
-for row in data:
-	with conn.cursor() as cur:
-		sql = 'insert into mostLinkedMissingNew(namespace, title, value) values(' + str(row[0]) + ', "' + row[1].replace('"', '\\"') + '", ' + str(row[2]) + ');'
-		print sql
-		cur.execute(sql)
+# Get the data from database
+conn = db.connect('cswiki')
+with conn.cursor() as cur:
+	sql = "SELECT pl_namespace AS namespace, pl_title AS title, COUNT(*) AS value FROM pagelinks LEFT JOIN page AS pg1 ON pl_namespace = pg1.page_namespace AND pl_title = pg1.page_title LEFT JOIN page AS pg2 ON pl_from = pg2.page_id WHERE pg1.page_namespace IS NULL AND pl_namespace NOT IN ( 2, 3 ) AND pg2.page_namespace NOT IN ( 8, 10 ) GROUP BY pl_namespace, pl_title ORDER BY value;"
+	cur.execute(sql)
+	tconn = db.connect('s53348__mostLinkedMissing')
+	for row in ResultIter(cur):
+		with tconn.cursor() as cur2:
+			sql = 'insert into mostLinkedMissingNew(namespace, title, value) values(' + str(row[0]) + ', "' + row[1].replace('"', '\\"') + '", ' + str(row[2]) + ');'
+			print sql
+			cur2.execute(sql)
